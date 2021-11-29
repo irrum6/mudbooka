58df329b4f1bd4504f84cca16185d93417cea87d
irrum6 <irrum6@github.com>
2021-11-29 23:18:45 +0400

    custom input for interval and keepfor

--- background.js
+++ background.js
@@ -3,8 +3,8 @@ const MINUTE = SECOND * 60;
 const HOUR = SECOND * 3600;
 const DAY = HOUR * 24;
 
-const VALID_INTERVALS = ["1h", "2h", "3h", "4h", "5h", "6h"];
-const VALID_KEEPS = ["12h", "1d", "2d", "3d", "4d", "5d"];
+const VALID_INTERVALS = ["1h", "2h", "3h", "4h", "5h", "6h", "7h", "8h"];
+const VALID_KEEPS = ["12h", "1d", "2d", "3d", "4d", "5d", "6d", "7d"];
 Object.freeze(VALID_INTERVALS);
 Object.freeze(VALID_KEEPS);
 
@@ -20,6 +20,8 @@ const input_to_interval = value => {
     return HOUR * parsed;
 }
 
+const parse_interval_range = value => MINUTE * Number(value);
+
 const input_to_keepfor = value => {
     let returnval = HOUR * 48;
     if (!VALID_KEEPS.includes(value)) {
@@ -27,9 +29,6 @@ const input_to_keepfor = value => {
         return returnval;
     }
     switch (value) {
-        case "6h":
-            returnval = HOUR * 6;
-            break;
         case "12h":
             returnval = HOUR * 12;
             break;
@@ -38,6 +37,8 @@ const input_to_keepfor = value => {
         case "3d":
         case "4d":
         case "5d":
+        case "6d":
+        case "7d":
             let replaced = value.replace("d", "");
             returnval = HOUR * 24 * Number(replaced);
             break;
@@ -46,6 +47,8 @@ const input_to_keepfor = value => {
     }
     return returnval;
 }
+
+const parse_keepfor_range = value => HOUR * Number(value);
 
 const randstr = () => Array.prototype.map.call(window.crypto.getRandomValues(new Uint32Array(2)), e => e.toString("36")).join("");
 
@@ -66,11 +69,6 @@ let runner = async () => {
 
     const folder_id = folder.id;
 
-    for (const t of tabs) {
-        const { title, url } = t;
-        await browser.bookmarks.create({ parentId: folder_id, title, url });
-    }
-
     //delete old items
     let folders = await browser.bookmarks.search({ query: prefix });
     let early_date = new Date(Date.now() - keepfor);
@@ -82,24 +80,47 @@ let runner = async () => {
         }
     }
 
+    for (const t of tabs) {
+        const { title, url } = t;
+        await browser.bookmarks.create({ parentId: folder_id, title, url });
+    }
+
 }
 
 let runner_id = window.setInterval(runner, interval);
 
-browser.storage.onChanged.addListener(async () => {
-    //console.log("soc");
-    window.clearInterval(runner_id);
-
+const load_and_set_interval = async () => {
     let got_interval = await browser.storage.local.get("interval");
     if (undefined !== got_interval) {
-        interval = input_to_interval(got_interval.interval);
+        let _value = got_interval.interval;
+        if ("c" === _value) {
+            let _range = await browser.storage.local.get("custom_interval");
+            interval = parse_interval_range(_range.custom_interval);
+        } else {
+            interval = input_to_interval(_value);
+        }
     }
+}
+
+const load_and_set_keepfor = async () => {
     let got_keepfor = await browser.storage.local.get("keepfor");
     if (undefined !== got_keepfor) {
-        keepfor = input_to_keepfor(got_keepfor.keepfor);
+        let _value = got_keepfor.keepfor;
+        if ("c" === _value) {
+            let _range = await browser.storage.local.get("custom_keepfor");
+            keepfor = parse_keepfor_range(_range.custom_keepfor);
+        } else {
+            keepfor = input_to_keepfor(_value);
+        }
     }
-    console.log(got_interval, got_keepfor);
-    console.log(interval, keepfor);
+}
+
+browser.storage.onChanged.addListener(async () => {
+    window.clearInterval(runner_id);
+    // load and set interval
+    await load_and_set_interval();
+    //load and set keepfor
+    await load_and_set_keepfor();
     runner_id = window.setInterval(runner, interval);
 });
 
--- options/options.js
+++ options/options.js
@@ -4,22 +4,95 @@ const query_all = s => document.body.querySelectorAll(s);
 
 const get_input_value = input_name => query(`input[name=${input_name}]`).value;
 
-set_interval_value = data => {
+const set_interval_value = data => {
     if (undefined === data || null === data || undefined === data.interval) {
         return;
     }
     query(`[data-int='${data.interval}']`).checked = true;
+    if ("c" === data.interval) {
+        toggleIntervalRangeValueEnabledState(true);
+    }
 }
-set_keep_value = data => {
+const set_interval_range_value = data => {
+    if (undefined === data || null === data || undefined === data.custom_interval) {
+        return;
+    }
+    query("#interval_range").value = data.custom_interval;
+    query("#display_interval_range").textContent = data.custom_interval;
+}
+
+const set_keep_value = data => {
     if (undefined === data || null === data || undefined === data.keepfor) {
         return;
     }
     query(`[data-keep='${data.keepfor}']`).checked = true;
+    if ("c" === data.keepfor) {
+        toggleKeepForRangeValueEnabledState(true);
+    }
+}
+
+const set_keepfor_range_value = data => {
+    if (undefined === data || null === data || undefined === data.custom_keepfor) {
+        return;
+    }
+    query("#keepfor_range").value = data.custom_keepfor;
+    query("#display_keepfor_range").textContent = data.custom_keepfor;
+}
+
+
+// interval range functions 
+const onIntervalRangeValueChange = e => {
+    query("#display_interval_range").textContent = e.target.value;
+}
+query("#interval_range")[on]("change", onIntervalRangeValueChange);
+query("#interval_range")[on]("input", onIntervalRangeValueChange);
+
+const toggleIntervalRangeValueEnabledState = v => {
+    if (true === v) {
+        query("#interval_range").disabled = false;
+        return;
+    }
+    query("#interval_range").disabled = true;
+}
+
+const interval_inputs = query_all("input[name=interval]");
+for (const input of interval_inputs) {
+    input[on]("change", e => {
+        if ("c" === e.target.value) {
+            toggleIntervalRangeValueEnabledState(true);
+            return;
+        }
+        toggleIntervalRangeValueEnabledState(false);
+    })
+}
+// keepfor range functions 
+const onKeepForRangeValueChange = e => {
+    query("#display_keepfor_range").textContent = e.target.value;
+}
+query("#keepfor_range")[on]("change", onKeepForRangeValueChange);
+query("#keepfor_range")[on]("input", onKeepForRangeValueChange);
+
+const toggleKeepForRangeValueEnabledState = v => {
+    if (true === v) {
+        query("#keepfor_range").disabled = false;
+        return;
+    }
+    query("#keepfor_range").disabled = true;
+}
+
+const keepfor_inputs = query_all("input[name=keepfor]");
+for (const input of keepfor_inputs) {
+    input[on]("change", e => {
+        if ("c" === e.target.value) {
+            toggleKeepForRangeValueEnabledState(true);
+            return;
+        }
+        toggleKeepForRangeValueEnabledState(false);
+    })
 }
 
 /**
  * Retrieves input values and sets interval and keepfor values
- * @param {Event} e 
  */
 const onSetParameters = async () => {
     let interval = "1h";
@@ -32,13 +105,25 @@ const onSetParameters = async () => {
     if (keeper !== undefined && keeper !== null) {
         keepfor = keeper.value;
     }
+    let custom_interval = "60";
+    let custom_keepfor = "48";
 
-    await browser.storage.local.set({ interval, keepfor });
+    if ("c" === interval) {
+        custom_interval = query("#interval_range").value;
+    }
+    if ("c" === keepfor) {
+        custom_keepfor = query("#keepfor_range").value;
+    }
+
+    console.log(custom_interval, custom_keepfor);
+
+    await browser.storage.local.set({ interval, keepfor, custom_interval, custom_keepfor });
 }
 
-// runner();
 query("#action")[on]("click", onSetParameters);
 
 //set input values from storage
-browser.storage.local.get("interval").then(set_interval_value)
-browser.storage.local.get("keepfor").then(set_keep_value)
+browser.storage.local.get("interval").then(set_interval_value);
+browser.storage.local.get("keepfor").then(set_keep_value);
+browser.storage.local.get("custom_interval").then(set_interval_range_value);
+browser.storage.local.get("custom_keepfor").then(set_keepfor_range_value);
