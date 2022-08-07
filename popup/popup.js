f9af6ef10b17268e5b13811bb11a4ce1ebaa8da7
irakli mindorashvili <irrum6@github.com>
2022-08-08 01:48:55 +0400

    added window count in popup

--- popup/popup.js
+++ popup/popup.js
@@ -18,5 +18,18 @@ browser.storage.local.get(["last", "next"]).then(async (data) => {
     query("#next").textContent = next_date.toLocaleString();
 
     let tabs = await browser.tabs.query({});
+    let windows = await browser.windows.getAll();
+    let nonprivates = 0;
+    let privates = 0;
+    for (const w of windows) {
+        if (w.incognito) {
+            privates += 1;
+            continue;
+        }
+        nonprivates += 1;
+    }
     query("#open_tabs").textContent = tabs.length;
+    query("#open_windows").textContent = windows.length;
+    query("#norm").textContent = nonprivates;
+    query("#private").textContent = privates;
 });
